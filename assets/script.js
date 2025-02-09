2af5c654d5630653b03ba9af1c53edbc01deb402
iam-sahil <sss.lata82@gmail.com>
2025-02-09 19:49:22 +0530

    testing navbar and landing

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll', function() {
+    var bodyElement = document.body;
+    var panelElements = document.querySelectorAll('.panel');
+  
+    var scroll = window.scrollY + (window.innerHeight / 5);
+    panelElements.forEach(function(panel) {
+      var panelElement = panel;
+  
+      if (panelElement.offsetTop <= scroll && panelElement.offsetTop + panelElement.offsetHeight > scroll) {
+
+        bodyElement.classList.forEach(function(className) {
+          if (className.startsWith('color-')) {
+            bodyElement.classList.remove(className);
+          }
+        });
+        var colorData = panelElement.dataset.color;
+        if (colorData) {
+          bodyElement.classList.add('color-' + colorData);
+        }
+      }
+    });
+  
+  });
+window.dispatchEvent(new Event('scroll'));
