d4bf92d50de9c15e60c977952061ab87b6f5abec
iam-sahil <sss.lata82@gmail.com>
2025-02-11 20:58:17 +0530

    new homepage

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,14 @@ window.addEventListener('scroll', function() {
   });
 window.dispatchEvent(new Event('scroll'));
 
+function Marquee(selector, speed) {
+  const marqueeElements = document.querySelectorAll(selector);
+
+    marqueeElements.forEach(marqueeElement => {
+    let i = 0, interval;
+    const firstElement = marqueeElement.children[0];
+    const start = () => interval = setInterval(() => firstElement.style.marginLeft = `-${(i += speed) % firstElement.clientWidth}px`, 10);
+    start();
+  });
+}
+window.addEventListener('load', () => Marquee('.marquee', .7));
